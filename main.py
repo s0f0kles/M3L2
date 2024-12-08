24fa6e741b637647ef27ca4496ec2748c408a5e0
s0f0kles <sofya.revo@gmail.com>
2024-12-08 11:32:49 +0100

    New functions

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from config import database
 
 if __name__ == '__main__':
     manager = DB_Manager(database)
-    manager.create_tables()
+   # manager.create_tables()
+    manager.default_insert()
+    manager.insert_project([(1, 'Tsyplonok_kjutBot', 'https://github.com/s0f0kles/Tsyplonok_kjutBot.git', 5)])
+    manager.update_projects('descripcion', ('Telegram bot'), 'Tsyplonok_kjutBot', 1)
